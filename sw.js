c23f04b41d3bf32e8859662d8ab7a86ab8cf5f1f
WhiteHorse73 <pashka6092006@gmail.com>
2026-07-06 12:32:54 +0300

    Increment cache version from v29 to v30
    Updated cache version to ensure cache invalidation.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ВЕРСІЯ КЕШУ. Збільшуй число (v4, v5...) щоразу, коли треба гарантовано скинути кеш. */
-const VERSION = 'pc-v29';
+const VERSION = 'pc-v30';
 const CORE = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', e => {
