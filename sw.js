492d965add7d2055a2cc328a9f501f9326684a2d
WhiteHorse73 <pashka6092006@gmail.com>
2026-07-08 12:22:30 +0300

    Update cache version from v36 to v37
    Increment cache version to ensure cache reset.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ВЕРСІЯ КЕШУ. Збільшуй число (v4, v5...) щоразу, коли треба гарантовано скинути кеш. */
-const VERSION = 'pc-v36';
+const VERSION = 'pc-v37';
 const CORE = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', e => {
