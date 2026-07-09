4bd9da809fa717d878480bace13d50bf9d53fdb1
WhiteHorse73 <pashka6092006@gmail.com>
2026-07-09 12:58:44 +0300

    Update cache version to pc-v39
    Increment cache version to ensure cache is updated.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ВЕРСІЯ КЕШУ. Збільшуй число (v4, v5...) щоразу, коли треба гарантовано скинути кеш. */
-const VERSION = 'pc-v38';
+const VERSION = 'pc-v39';
 const CORE = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', e => {
