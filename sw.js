8c9fb0ad23f9a535c67be966332eaa1df8d77b07
WhiteHorse73 <pashka6092006@gmail.com>
2026-07-15 12:46:45 +0300

    Increase cache version from v46 to v49
    Updated cache version to ensure cache invalidation.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ВЕРСІЯ КЕШУ. Збільшуй число (v4, v5...) щоразу, коли треба гарантовано скинути кеш. */
-const VERSION = 'pc-v46';
+const VERSION = 'pc-v49';
 const CORE = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', e => {
